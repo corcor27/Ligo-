4bca243b8538d505afd1ca155812635fec61c649
corcor27 <cory13579@googlemail.com>
2017-08-21 09:46:56 +0100

    Update count.py

--- count.py
+++ count.py
@@ -2,7 +2,7 @@ from collections import Counter
 def main():
     #use open() for opening file.
     #Always use `with` statement as it'll automatically close the file for you.
-    with open(r'home/cory\Documents\posteriors_samples.txt') as f:
+    with open(r'home/cory/Documents/posteriors_samples.txt') as f:
         #create a list of all words fetched from the file using a list comprehension
         words = [word for line in f for word in line.split()]
         print "The total word count is:", len(words)
