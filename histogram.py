89f68fca95059d20a3946324dcc32a49f281e012
corcor27 <cory13579@googlemail.com>
2017-08-22 11:17:57 +0100

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -6,18 +6,15 @@ test=(1,2.3,3,4,5,6,6,7,7,88,9,)
 
 #open file from which we want to read data
 #for this to work, we must have our python file saved in the same directory as the data file of interest
-g= open('parameters.txt', 'r')
+
 
 f = open('run32samples.txt', 'r')
 
     
     #create empty list to store numerics of interest
-parameters = []
+
 data = []
-#use this to search for parameters
-for line in g:
-        parameters.append(line.split())
-    
+
 #m1=24th index
 #m2 = 27th index    
     
@@ -32,16 +29,17 @@ for line in f:
 #    chi_p = [x[46] for x in data ]
 #    chi_eff = [x[46] for x in data ]
 #plt.figure(1)
-#plt.hist(m1_freq,50, normed=True)
-#plt.xlabel('m1')
-#plt.ylabel('probability density')
-
+plt.hist(m1_freq,50, normed=True)
+plt.xlabel('m1')
+plt.ylabel('probability density')
+plt.axis([70, 95, 0, 0.16])
+plt.savefig("Run32_attempt2_mass1.png")
 #plt.figure(2)
 #plt.hist(m2_freq,50, normed=True)
 #plt.xlabel('m2')
 #plt.ylabel('probability density')
 
-plt.figure()
-plt.hist(chi_p,50, normed=True)
-plt.xlabel(r'$\chi_p$')
-plt.ylabel('probability density')
+#plt.figure()
+#plt.hist(chi_p,50, normed=True)
+#plt.xlabel(r'$\chi_p$')
+#plt.ylabel('probability density')
