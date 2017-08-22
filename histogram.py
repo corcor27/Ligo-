fe5f9a6fbd9cda372fe7bbd0875927b6b72d5562
corcor27 <cory13579@googlemail.com>
2017-08-22 11:11:02 +0100

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -27,9 +27,9 @@ for line in g:
 for line in f:
     data.append([float(x) for x in line.split()])
 #add parameters as required
-#    m1_freq= [ x[24] for x in data]
+     m1_freq= [ x[24] for x in data]
 #    m2_freq =[x[27] for x in data ]
-    chi_p = [x[46] for x in data ]
+#    chi_p = [x[46] for x in data ]
 #    chi_eff = [x[46] for x in data ]
 #plt.figure(1)
 #plt.hist(m1_freq,50, normed=True)
