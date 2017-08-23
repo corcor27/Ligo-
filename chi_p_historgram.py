fe3c9470907afc67f586ccee0be2eee675735327
corcor27 <cory13579@googlemail.com>
2017-08-23 19:53:40 +0100

    Update chi_p_historgram.py

--- chi_p_historgram.py
+++ chi_p_historgram.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 #open file from which we want to read data
 #for this to work, we must have our python file saved in the same directory as the data file of interest
 
-g = open('run35_samples.txt', 'r')
+g = open('run22sample.txt', 'r')
 
     
     #create empty list to store numerics of interest
@@ -19,7 +19,7 @@ for line in g:
 #add parameters as required
 #    m1_freq= [ x[28] for x in data]
 #    m2_freq =[x[31] for x in data ]
-chi_p = [x[50] for x in data ]
+chi_p = [x[52] for x in data ]
 #    chi_eff = [x[46] for x in data ]
 #plt.figure(1)
 
