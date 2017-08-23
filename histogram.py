cd12af2040a63b49e40adb7e791588d68c471214
corcor27 <cory13579@googlemail.com>
2017-08-23 19:42:13 +0100

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -7,7 +7,7 @@ test=(1,2.3,3,4,5,6,6,7,7,88,9,)
 #open file from which we want to read data
 #for this to work, we must have our python file saved in the same directory as the data file of interest
 
-g = open('run32samples.txt', 'r')
+g = open('run22_samples.txt', 'r')
 
     
     #create empty list to store numerics of interest
@@ -25,7 +25,7 @@ for line in g:
 #add parameters as required
 #    m1_freq= [ x[28] for x in data]
 #    m2_freq =[x[31] for x in data ]
-chi_p = [x[47] for x in data ]
+chi_p = [x[51] for x in data ]
 #    chi_eff = [x[46] for x in data ]
 #plt.figure(1)
 
@@ -37,10 +37,10 @@ lower_90=np.percentile(chi_p, 5)
 plt.hist(chi_p,50, facecolor='green', normed=True)
 plt.xlabel('chi_p')
 plt.axvline(x=lower_90,linewidth=2,linestyle='dashed',color='m')
-plt.axvline(x=0.75,linewidth=2, color='r')
+plt.axvline(x=0.5,linewidth=2, color='r')
 plt.axvline(x=upper_90,linewidth=2,linestyle='dashed',color='m')
 plt.ylabel('probability density')
-plt.axis([0.3, 0.9, 0, 8.0])
+plt.axis([0, 1, 0, 8.0])
 plt.savefig("Run32_attempt9_chi_p.png")
 #plt.figure(2)
 #plt.hist(m2_freq,50, normed=True)
