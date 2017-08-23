425e63e77cab20b8185e71c7e888f1e6471a7efa
corcor27 <cory13579@googlemail.com>
2017-08-23 22:08:54 +0100

    Update m1_historgram.py

--- m1_historgram.py
+++ m1_historgram.py
@@ -37,9 +37,9 @@ plt.axvline(x=Lal_upper_90,linewidth=2,linestyle='dashed',color='m')
 #plt.axvline(x=pycbc_lower_90,linewidth=2,linestyle='dashed',color='k')
 #plt.axvline(x=pycbc_upper_90,linewidth=2,linestyle='dashed',color='k')
 plt.axvline(x=80,linewidth=2, color='r')
-plt.axis([65, 95, 0, 8.0])
+plt.axis([65, 95, 0, 1])
 plt.ylabel('probability density')
-plt.savefig("Run32_m1_lal.png")
+plt.savefig("Run32_m1_lal_2.png")
 
 
 
