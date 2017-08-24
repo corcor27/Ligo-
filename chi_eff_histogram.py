23d8239ac68175a58ac7627e1f4dd049e20c74c4
corcor27 <cory13579@googlemail.com>
2017-08-24 09:51:09 +0100

    Update chi_eff_histogram.py

--- chi_eff_histogram.py
+++ chi_eff_histogram.py
@@ -19,7 +19,7 @@ for line in g:
 #add parameters as required
 #    m1_freq= [ x[28] for x in data]
 #    m2_freq =[x[31] for x in data ]
-chi_p = [x[28] for x in data ]
+chi_p = [x[69] for x in data ]
 #    chi_eff = [x[46] for x in data ]
 #plt.figure(1)
 
@@ -31,15 +31,15 @@ Lal_lower_90=np.percentile(chi_p, 5)
 #pycbc_lower_90=np.percentile(pycbc_data, 5)
 plt.hist(chi_p,50, facecolor='green', normed=True)
 #plt.hist(pycbc_data,50, normed=True, color='b')
-plt.xlabel('m1')
+plt.xlabel('chi_eff')
 plt.axvline(x=Lal_lower_90,linewidth=2,linestyle='dashed',color='m')
 plt.axvline(x=Lal_upper_90,linewidth=2,linestyle='dashed',color='m')
 #plt.axvline(x=pycbc_lower_90,linewidth=2,linestyle='dashed',color='k')
 #plt.axvline(x=pycbc_upper_90,linewidth=2,linestyle='dashed',color='k')
-plt.axvline(x=80,linewidth=2, color='r')
-plt.axis([65, 95, 0, 0.4])
+plt.axvline(x=0.44,linewidth=2, color='r')
+plt.axis([0, 25, 0, 1])
 plt.ylabel('probability density')
-plt.savefig("Run32_m1_lal2.png")
+plt.savefig("Run32_chi_eff_lal.png")
 
 
 
