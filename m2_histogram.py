39eec06a4137f890c1ce593664dbfd3596fdecbe
corcor27 <cory13579@googlemail.com>
2017-08-24 09:08:07 +0100

    Update m2_histogram.py

--- m2_histogram.py
+++ m2_histogram.py
@@ -37,9 +37,9 @@ plt.axvline(x=Lal_upper_90,linewidth=2,linestyle='dashed',color='m')
 #plt.axvline(x=pycbc_lower_90,linewidth=2,linestyle='dashed',color='k')
 #plt.axvline(x=pycbc_upper_90,linewidth=2,linestyle='dashed',color='k')
 plt.axvline(x=10,linewidth=2, color='r')
-plt.axis([0, 20, 0, 0.6])
+plt.axis([5, 15, 0, 0.8])
 plt.ylabel('probability density')
-plt.savefig("Run32_m2_lal3.png")
+plt.savefig("Run32_m2_lal4.png")
 
 
 
