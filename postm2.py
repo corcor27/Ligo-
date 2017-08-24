d541cfb93829ca61206f1cb4a6470151861766b8
corcor27 <cory13579@googlemail.com>
2017-08-24 10:04:25 +0100

    Update postm2.py

--- postm2.py
+++ postm2.py
@@ -179,7 +179,7 @@ def plotPosterior(parameter):
    plt.axvline(x=lower_90,linewidth=2,linestyle='dashed',color='k')
    plt.axvline(x=10,linewidth=2, color='r')
    plt.axvline(x=upper_90,linewidth=2,linestyle='dashed',color='k')
-   plt.axis([6, 14, 0, 1])
+   plt.axis([0.3, 0.6, 0, 25])
    plt.xlabel("%s" % parameter)
    plt.grid()
    # Removed the priors, add them if you fancy
