a4a3c31edcd4ad28d13b6b67f4161da8a149e3cf
corcor27 <cory13579@googlemail.com>
2017-08-27 12:28:54 +0100

    Create data_input.py

---
+++ data_input.py
@@ -0,0 +1,30 @@
+from __future__ import division
+import numpy as np
+
+
+#open file from which we want to read data
+#for this to work, we must have our python file saved in the same directory as the data file of interest
+
+g = open('run87_data.txt', 'r')
+
+    
+    #create empty list to store numerics of interest
+
+data = []
+
+
+
+for line in g:
+    data.append([float(x) for x in line.split()])
+#add parameters as required
+#    m1_freq= [ x[28] for x in data]
+#    m2_freq =[x[31] for x in data ]
+chi_p = [x[52] for x in data ]
+#    chi_eff = [x[46] for x in data ]
+#plt.figure(1)
+
+
+Lal_upper_90=np.percentile(chi_p, 95)
+Lal_lower_90=np.percentile(chi_p, 5)
+mean_val=np.average(chi_p)
+print(Lal_upper_90,Lal_lower_90,mean_val
