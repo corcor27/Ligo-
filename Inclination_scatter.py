80a5a3fd2ae0c745850503f9c60692a8fb54bb72
corcor27 <cory13579@googlemail.com>
2017-08-27 16:46:12 +0100

    Update Inclination_scatter.py

--- Inclination_scatter.py
+++ Inclination_scatter.py
@@ -6,14 +6,15 @@ I = [90.0, 60.0, 45.0, 30.0, 22.5]
 ae = [48.30, 36.24, 23.48, 28.46, 67.66]
 be = [0.0, 7.78, 15.9, 23.36, 18.88]
 ce = [47.74, 15.38, 28.48, 0.0, 22.86]
-plt.errorbar(x=I, y=a, yerr=ae, fmt='o', color='g', label = 'M70_q6_inclination_spin1x=0.5')
-plt.errorbar(x=I, y=b, yerr=be, fmt='>', color='b', label = 'M70_q6_inclination_spin1x=0.5')
-plt.errorbar(x=I, y=c, yerr=ce, fmt='x', color='r', label = 'M70_q6_inclination_spin1x=0.5')
+plt.errorbar(x=I, y=a, yerr=ae, fmt='o', color='g', label = 'M70_q6_spin1x=0.5')
+plt.errorbar(x=I, y=b, yerr=be, fmt='>', color='b', label = 'M80_q7_spin1x=0.5')
+plt.errorbar(x=I, y=c, yerr=ce, fmt='x', color='r', label = 'M90_q8_spin1x=0.5')
 #plt.plot(I, a, linewidth=2,linestyle='dashed',color='g')
 #plt.plot(I, b, linewidth=2,linestyle='dashed',color='b')
 #plt.plot(I, c, linewidth=2,linestyle='dashed',color='r')
 plt.title("Estimation of chi_p with varying inclination")
 plt.xlabel('Inclination (degrees)')
 plt.ylabel('Percentage error')
+plt.legend(loc='upper left', fontsize=10.5)
 plt.axis([20, 100, -80, 100])
 plt.savefig("Inc_precent_error8.png")
