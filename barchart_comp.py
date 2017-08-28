69c84ea5e6e90be03030f289e6f009f921159ff5
corcor27 <cory13579@googlemail.com>
2017-08-28 17:17:28 +0100

    Update barchart_comp.py

--- barchart_comp.py
+++ barchart_comp.py
@@ -18,17 +18,17 @@ width = 0.35       # the width of the bars
 
 #run22
 
-lal_v=(2.893, 5.13, 24.8, 30.88, 1.521, 8.43, 64.4)
-lal_e= (2.48, 13.28, 48.2, 42.647, 4.371, 22.891, 78.8)
-pycbc_v = (6.26, 8.89, 79.6, 35.29, 1.913, 16.867, 40.2)
-pycbc_e = (2.27, 19.64, 71.44, 49.706, 4.57, 39.75, 57.4)  
+#lal_v=(2.893, 5.13, 24.8, 30.88, 1.521, 8.43, 64.4)
+#lal_e= (2.48, 13.28, 48.2, 42.647, 4.371, 22.891, 78.8)
+#pycbc_v = (6.26, 8.89, 79.6, 35.29, 1.913, 16.867, 40.2)
+#pycbc_e = (2.27, 19.64, 71.44, 49.706, 4.57, 39.75, 57.4)  
 
 #run31
 
-#lal_v=(1.105, 9.74, 8.8, 37, 4.85, 11.2, 66.8)
-#lal_e= (6.43, 24.98, 84.8, 78.5, 12.35, 32, 123.2)
-#pycbc_v = (2.805, 15.38, 84.4, 51.5, 7.19, 18.4, 64.4)
-#pycbc_e = (3.712, 24.78, 77.2, 77.5, 10.70, 36, 86.8)  
+lal_v=(1.105, 9.74, 8.8, 37, 4.85, 11.2, 66.8)
+lal_e= (6.43, 24.98, 84.8, 78.5, 12.35, 32, 123.2)
+pycbc_v = (2.805, 15.38, 84.4, 51.5, 7.19, 18.4, 64.4)
+pycbc_e = (3.712, 24.78, 77.2, 77.5, 10.70, 36, 86.8)  
 
 
 
@@ -43,10 +43,10 @@ rects2 = ax.bar(ind + width, pycbc_v, width, color='b', yerr=pycbc_e, label = 'P
 
 # add some text for labels, title and axes ticks
 ax.set_ylabel('Percentage Error')
-ax.set_title('Lal v Pycbc-Run3')
+ax.set_title('Lal v Pycbc-Run4')
 ax.set_xticks(ind + width / 2)
 ax.set_xticklabels(('m1', 'm2', 'chi_p', 'chi_eff', 'Mc','q', 'spin1'))
-plt.ylim((0,180))
+plt.ylim((0,200))
 plt.xlabel('Parameters')
 plt.legend(loc='upper left', fontsize=10.5)
-plt.savefig("Run3_comparsions.png")
+plt.savefig("Run4_comparsions.png")
