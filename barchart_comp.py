a5f06fad4e5a886d62d9b8e7a3c807c9df01d373
corcor27 <cory13579@googlemail.com>
2017-08-28 16:25:53 +0100

    Update barchart_comp.py

--- barchart_comp.py
+++ barchart_comp.py
@@ -2,19 +2,19 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
-pram =['m1', 'm2', 'chi_p', 'chi_eff', 'Mc', 'Distance', 'q', 'spin1']
+pram =['m1', 'm2', 'chi_p', 'chi_eff', 'Mc','q', 'spin1']
 
-
+ind = np.arange(N)  # the x locations for the groups
+width = 0.35       # the width of the bars
   
 
-N=9
+N=7
 lal_v=(8.322, 16.185, 5.2, 5.607, 29.673, 28.070, 50)
 
 lal_e= (2.06, 33.83, 42, 29.673, 10.837, 58.596, 82.8)
 
 
-ind = np.arange(N)  # the x locations for the groups
-width = 0.35       # the width of the bars
+
 
 fig, ax = plt.subplots()
 rects1 = ax.bar(ind, lal_v, width, color='g', yerr=lal_e)
@@ -29,7 +29,7 @@ rects2 = ax.bar(ind + width, pycbc_v, width, color='b', yerr=pycbc_e)
 ax.set_ylabel('Percentage Error')
 ax.set_title('Lal v Pycbc')
 ax.set_xticks(ind + width / 2)
-ax.set_xticklabels(('m1', 'm2', 'chi_p', 'chi_eff', 'Mc', 'Distance', 'q', 'spin1'))
+ax.set_xticklabels(('m1', 'm2', 'chi_p', 'chi_eff', 'Mc','q', 'spin1'))
 
 autolabel(rects1)
 autolabel(rects2)
