b39be379913de2f12f81ca6d6ddf97938d9fbf80
corcor27 <cory13579@googlemail.com>
2017-08-31 14:21:34 +0100

    Update m1_historgram.py

--- m1_historgram.py
+++ m1_historgram.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 #open file from which we want to read data
 #for this to work, we must have our python file saved in the same directory as the data file of interest
 
-g = open('run32samples.txt', 'r')
+g = open('run89_data.txt', 'r')
 
     
     #create empty list to store numerics of interest
@@ -19,7 +19,7 @@ for line in g:
 #add parameters as required
 #    m1_freq= [ x[28] for x in data]
 #    m2_freq =[x[31] for x in data ]
-chi_p = [x[28] for x in data ]
+chi_p = [x[61] for x in data ]
 #    chi_eff = [x[46] for x in data ]
 #plt.figure(1)
 
@@ -29,17 +29,17 @@ Lal_upper_90=np.percentile(chi_p, 95)
 Lal_lower_90=np.percentile(chi_p, 5)
 #pycbc_upper_90=np.percentile(pycbc_data, 95)
 #pycbc_lower_90=np.percentile(pycbc_data, 5)
-plt.hist(chi_p,50, facecolor='green', normed=True)
+plt.hist(chi_p,50, facecolor='green', normed=True, 'Lal')
 #plt.hist(pycbc_data,50, normed=True, color='b')
-plt.xlabel('m1')
-plt.axvline(x=Lal_lower_90,linewidth=2,linestyle='dashed',color='m')
+plt.xlabel('mc')
+plt.axvline(x=Lal_lower_90,linewidth=2,linestyle='dashed',color='m',label = '90% interval')
 plt.axvline(x=Lal_upper_90,linewidth=2,linestyle='dashed',color='m')
 #plt.axvline(x=pycbc_lower_90,linewidth=2,linestyle='dashed',color='k')
 #plt.axvline(x=pycbc_upper_90,linewidth=2,linestyle='dashed',color='k')
-plt.axvline(x=80,linewidth=2, color='r')
-plt.axis([65, 95, 0, 0.4])
+plt.axvline(x=21.2,linewidth=2, color='r',label = 'Injected value')
+plt.axis([15, 30, 0, 0.8])
 plt.ylabel('probability density')
-plt.savefig("Run32_m1_lal2.png")
+plt.savefig("Run89-mc.png")
 
 
 
