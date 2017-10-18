aa8ff0b7609cd701d773ef94d4900e30fa1a8013
corcor27 <cory13579@googlemail.com>
2017-10-18 19:29:03 +0100

    Update Mulityplot.py

--- Mulityplot.py
+++ Mulityplot.py
@@ -28,8 +28,8 @@ def chi_p_1_90PE():
     saved_chi_p_1 = [x[52] for x in data ]
     upper_90_1=np.percentile(saved_chi_p_1, 95)
     mean_val_1=np.average(saved_chi_p_1)
-    90PE_1 = (abs(upper_90_1 - injected_value) / injected_value) * 100
-    return 90PE_1
+    90_PE_1 = (abs(upper_90_1 - injected_value) / injected_value) * 100
+    return 90_PE_1
 
 print(chi_p_1_MPE,chi_p_1_90PE)
 
