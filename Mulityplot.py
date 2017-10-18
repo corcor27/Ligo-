e605e8259a669379ce878dbf8abc00959df85a05
corcor27 <cory13579@googlemail.com>
2017-10-18 19:42:37 +0100

    Update Mulityplot.py

--- Mulityplot.py
+++ Mulityplot.py
@@ -8,7 +8,7 @@ print "Initialising..."
 
 injected_value=0.5
 
-
+#manually set injection value
 
 def chi_p_1_MPE():
     data = []
@@ -22,7 +22,7 @@ def chi_p_1_MPE():
 
 def chi_p_1_90PE():
     data = []
-    g = open('run88_data.txt', 'r')
+    g = open('run89_data.txt', 'r')
     for line in g:
         data.append([float(x) for x in line.split()])
     saved_chi_p_1 = [x[52] for x in data ]
@@ -30,8 +30,35 @@ def chi_p_1_90PE():
     mean_val_1=np.average(saved_chi_p_1)
     PE_1 = (abs(upper_90_1 - injected_value) / injected_value) * 100
     return PE_1
+
+def chi_p_2_MPE():
+    data = []
+    f = open('run88_data.txt', 'r')
+    for line in f:
+        data.append([float(x) for x in line.split()])
+    saved_chi_p_2 = [x[52] for x in data ]
+    mean_val_2=np.average(saved_chi_p_2)
+    MPE_2 = (abs(mean_val_2 - injected_value) / injected_value) * 100
+    return MPE_2
+
+def chi_p_2_90PE():
+    data = []
+    g = open('run89_data.txt', 'r')
+    for line in f:
+        data.append([float(x) for x in line.split()])
+    saved_chi_p_2 = [x[52] for x in data ]
+    upper_90_2=np.percentile(saved_chi_p_2, 95)
+    mean_val_2=np.average(saved_chi_p_2)
+    PE_2 = (abs(upper_90_2 - injected_value) / injected_value) * 100
+    return PE_2
+
+
+
 run1MPE = chi_p_1_MPE()
 run1PE = chi_p_1_90PE()
-print(run1MPE,run1PE) 
+run2MPE = chi_p_2_MPE()
+run2PE = chi_p_2_90PE()
+
+print(run1MPE,run1PE,run2MPE,run2PE) 
    
 
